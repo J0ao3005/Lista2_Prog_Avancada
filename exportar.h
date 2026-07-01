3b0e11a2426d1df8d5b50a1e4e3a4b4ef4a13f6f
J0ao3005 <142702876+J0ao3005@users.noreply.github.com>
2026-07-01 17:14:34 -0300

    Add exportar.h for CSV export functionality

---
+++ exportar.h
@@ -0,0 +1,9 @@
+#ifndef EXPORTAR_H
+#define EXPORTAR_H
+
+#include "arvore.h"
+
+// Exporta todos os segmentos da árvore no formato CSV exigido pelo MiniCCO-1
+void exporta_csv_fisica(ptrNo raiz, const char *filename);
+
+#endif
